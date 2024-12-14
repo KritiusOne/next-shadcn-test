e084ed7b264e78938d288a71fcc579b29422f0e9
Julio David Rodríguez Ureche <julio10490@gmail.com>
2024-12-13 20:36:32 -0500

    Agregado: Prueba de temas en componente button y uso de sus clases en otro componente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
-
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 export default function Home() {
   return (
-    <h1>HOLA MUNDO</h1>
+    <div className="w-screen h-screen flex flex-row justify-center items-center gaap-2 gap-2">
+      <Button variant={"default"}>click</Button>
+      <Button variant={"destructive"}>click</Button>
+      <Button variant={"link"}>click</Button>
+      <Button variant={"secondary"}>click</Button>
+      <Button variant={"outline"}>click</Button>
+      <Button variant={"ghost"}>click</Button>
+      <Link href="/" className={ buttonVariants() } > go to home page</Link>
+    </div>
   );
 }
